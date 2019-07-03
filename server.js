8fe1853591f2d5ca04c8f84b742ead69cb0ab414
Matt Diamond <mattdiamond80@gmail.com>
2019-07-03 15:38:58 -0500

    Update signin route to check if email and password match db

--- server.js
+++ server.js
@@ -14,12 +14,6 @@ const db = knex({
   }
 });
 
-db.select("*")
-  .from("users")
-  .then(data => {
-    console.log(data);
-  });
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -58,15 +52,25 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    // res.json("success");
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash); // compare entered pw with hash
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch(err => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
