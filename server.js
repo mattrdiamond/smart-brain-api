0acdc30c47d09a65ed9dfa06f967a52573f0424d
Matt Diamond <mattdiamond80@gmail.com>
2019-07-03 11:58:44 -0500

    Update image endpoint to connect to db -> update entries

--- server.js
+++ server.js
@@ -71,8 +71,7 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  // register new user in database (knex)
-  return db("users")
+  return db("users") // register new user in database (knex)
     .returning("*") // users -> insert user and return all columns (knex)
     .insert({
       email: email,
@@ -80,7 +79,7 @@ app.post("/register", (req, res) => {
       joined: new Date()
     })
     .then(user => {
-      res.json(user[0]);
+      res.json(user[0]); // user returns user object in array. user[0] returns just object of registered user
     })
     .catch(err => res.status(400).json("unable to join"));
 });
@@ -105,15 +104,14 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) res.status(400).json("not found");
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
 });
 
 app.listen(3000, () => {
