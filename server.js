1d37f0d655e8c9dc845a4bbaeae38a019d6435a2
Matt Diamond <mattdiamond80@gmail.com>
2019-07-03 15:12:00 -0500

    Use Kinex transactions to insert hash and email inot login table then insert email, name and joined into users table and commit if both transactions pass

--- server.js
+++ server.js
@@ -71,17 +71,33 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  return db("users") // register new user in database (knex)
-    .returning("*") // users -> insert user and return all columns (knex)
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
-    })
-    .then(user => {
-      res.json(user[0]); // user returns user object in array. user[0] returns just object of registered user
-    })
-    .catch(err => res.status(400).json("unable to join"));
+  const hash = bcrypt.hashSync(password); // encrypt password
+  db.transaction(trx => {
+    // if transaction to one table fails, all transactions will fail - kinex
+    trx
+      .insert({
+        // first transaction - add hash and email to login table
+        hash: hash, // from bcrypt
+        email: email // from req.body
+      })
+      .into("login")
+      .returning("email")
+      .then(loginEmail => {
+        // second transaction - update users table with email
+        return trx("users") // register new user in database (knex)
+          .returning("*") // users -> insert user and return all columns (knex)
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]); // user returns user object in array. user[0] returns just object of registered user
+          });
+      })
+      .then(trx.commit) // if both transactions pass, then commit
+      .catch(trx.rollback); // any failure - db will rollback to prev state
+  }).catch(err => res.status(400).json("unable to join"));
 });
 
 app.get("/profile/:id", (req, res) => {
@@ -117,11 +133,3 @@ app.put("/image", (req, res) => {
 app.listen(3000, () => {
   console.log("running on port 3000");
 });
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT(update) --> user
-*/
